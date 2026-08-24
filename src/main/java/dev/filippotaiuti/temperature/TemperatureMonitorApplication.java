4292335bf540e5cc1930a09336ab449e24c0dc82
Filippo T. <ftaiuti@gmail.com>
2026-08-24 23:23:26 +0200

    initialize temperature monitor service

---
+++ src/main/java/dev/filippotaiuti/temperature/TemperatureMonitorApplication.java
@@ -0,0 +1,13 @@
+package dev.filippotaiuti.temperature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureMonitorApplication.class, args);
+	}
+
+}
